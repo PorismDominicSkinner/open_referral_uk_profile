e76a00e7e28bcc2869901dec511f24df57d936b0
PorismDominicSkinner <54143283+PorismDominicSkinner@users.noreply.github.com>
2023-06-07 17:22:08 +0100

    Create organization.js

---
+++ profile/organization.js
@@ -0,0 +1,12 @@
+{
+   "properties": {
+     "tax_status": null,
+     "tax_id": null,
+     "year_incorporated": null
+     "legal_status": null,
+     "legal_status": null,
+     "parent_organization_id": null
+   },
+   "allOf": [
+   ]
+}
