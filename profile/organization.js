61e17cde20b5de6ed559785d00fd5c52c8773506
PorismDominicSkinner <54143283+PorismDominicSkinner@users.noreply.github.com>
2023-06-07 17:29:17 +0100

    Update organization.js

--- profile/organization.js
+++ profile/organization.js
@@ -2,7 +2,7 @@
    "properties": {
      "tax_status": null,
      "tax_id": null,
-     "year_incorporated": null
+     "year_incorporated": null,
      "legal_status": null,
      "legal_status": null,
      "parent_organization_id": null
